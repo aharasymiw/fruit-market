ac8dad82e4d90c76ba57aaaa988487f1b72f792d
Andrew Harasymiw <aharasymiw@gmail.com>
2015-11-17 09:30:07 -0600

    made appleStore object and created apple buy button function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Global price variables, initiated to a base price
+var totalCash = 0;
 var applePrice = 500;
 var orangePrice = 500;
 var bananaPrice = 500;
 var grapePrice = 500;
+var appleStore = {
+	num: 0, 
+	avg: 0
+};
 
 // Return a random number between min and max
 function randomNumber(min, max) {
@@ -22,21 +27,15 @@ function randomPrice() {
 }
 
 // Update prices ever 15 seconds
-$(function(){
-    setInterval(function(){
-
-		// Initialize empty string variables from 
-		var appleString = '';
-		var orangeString = '';
-		var bananaString = '';
-		var grapeString = '';
-		
+function priceStream(){
+	setInterval(function(){
+	
 		// Get a random price adjustment for the fruit
 		applePrice += randomPrice();
 		orangePrice += randomPrice();
 		bananaPrice += randomPrice();
 		grapePrice += randomPrice();
-		
+	
 		// Update value of the global variable for the fruit's price
 		if(applePrice < 50){applePrice = 50;}
 		if(applePrice > 999){applePrice = 999;}
@@ -46,21 +45,31 @@ $(function(){
 		if(bananaPrice > 999){bananaPrice = 999;}		
 		if(grapePrice < 50){grapePrice = 50;}
 		if(grapePrice > 999){grapePrice = 999;}		
-		
-		// Convert the fruit's current price to a formated string for display
-		appleString = applePrice.toString().substring(0,1) + '.' + applePrice.toString().substring(1,3);
-		orangeString = orangePrice.toString().substring(0,1) + '.' + orangePrice.toString().substring(1,3);
-		bananaString = bananaPrice.toString().substring(0,1) + '.' + bananaPrice.toString().substring(1,3);
-		grapeString = grapePrice.toString().substring(0,1) + '.' + grapePrice.toString().substring(1,3);
-		
+	
 		//Update current price of the fruit in the DOM
 		$('#apple span').first().remove();
-		$('#apple > h3').append($('<span>').text(appleString));
+		$('#apple > h3').append($('<span>').text((applePrice/100).toFixed(2)));
 		$('#orange span').first().remove();
-		$('#orange > h3').append($('<span>').text(orangeString));
+		$('#orange > h3').append($('<span>').text((orangePrice/100).toFixed(2)));
 		$('#banana span').first().remove();
-		$('#banana > h3').append($('<span>').text(bananaString));
+		$('#banana > h3').append($('<span>').text((bananaPrice/100).toFixed(2)));
 		$('#grape span').first().remove();
-		$('#grape > h3').append($('<span>').text(grapeString));
-    }, 500)
+		$('#grape > h3').append($('<span>').text((grapePrice/100).toFixed(2)));
+	}, 500)
+}
+
+function buy(){
+	$('button').on('click', '#apple', function(){
+		
+		appleStore.num++;
+		 
+	})
+}
+// Document ready wraper
+$(function(){
+
+	priceStream();
+
+	// Button?
 }); 
+
